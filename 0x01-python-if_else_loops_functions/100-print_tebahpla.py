853839fe85589d1025407fd3d8b0f36ec147d8d7
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2022-09-21 08:00:10 +0100

    python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for var in range(ord('z'), ord('a') - 1, -1):
-    if var % 2 == 1:
-        var -= (ord('a') - ord('A'))
-    print("{:c}".format(var), end='')
+
+for i in range(0, 26):
+    if i % 2 == 0:
+        print("{:c}".format(122 - i), end="")
+    else:
+        print("{:c}".format(90 - i), end="")
